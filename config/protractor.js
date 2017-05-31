f974ae0172b3f3b949bcf0678348b2b9e41f6333
Angelo Brandão <angelobms@gmail.com>
2017-05-31 19:19:01 -0300

    Integração Sauce Labs

--- config/protractor.js
+++ config/protractor.js
@@ -1,6 +1,15 @@
 var config = require('./config');
 
 exports.config = {
+  sauceUser: config.sauceUser,
+  sauceKey: config.sauceKey,
+  capabilities: {
+    'name': config.sauceTestName,
+    'browserName': 'chrome',
+    'tunnel-identifier': config.travisJobNumber,
+    'build': config.travisBuild
+  },
+
   specs: ['../test/e2e/**/*.js'],
   onPrepare: function() {
     browser.driver.get('http://localhost:3000').then(function() {
